ff008eb169ec28720d9f1f7b9f7068cc9721997a
sk <sk_dev@posteo.de>
2018-09-24 15:13:00 +0200

    pad BoxNumber for VMax side, unpad for ologis side; release V1.1.2

--- rest-authentication/src/test/kotlin/com/github/skiedrowski/tools/rest/authentication/client/HTTPBasicAuthenticatorTest.kt
+++ rest-authentication/src/test/kotlin/com/github/skiedrowski/tools/rest/authentication/client/HTTPBasicAuthenticatorTest.kt
@@ -10,14 +10,34 @@ import javax.ws.rs.core.MultivaluedMap
 class HTTPBasicAuthenticatorTest {
     @Test
     fun `filter adds sensible Authorization header`() {
-        val authenticator = HTTPBasicAuthenticator("peter", "petersPassword")
+//        val authenticator = HTTPBasicAuthenticator("peter", "petersPassword")
+//
+//        val headers = mock<MultivaluedMap<String, Any>>()
+//        val requestContext = mock<ClientRequestContext> {
+//            on {this.headers} doReturn headers
+//        }
+//        authenticator.filter(requestContext)
+//        
+//        verify(headers).add("Authorization", "Basic cGV0ZXI6cGV0ZXJzUGFzc3dvcmQ=")
+        checkAuthHeader("peter", "petersPassword", "Basic cGV0ZXI6cGV0ZXJzUGFzc3dvcmQ=")
+    }
+
+    @Test
+    fun `filter adds sensible Authorization header 2`() {
+        checkAuthHeader("mauveS3", "bmx\$2+0-1=1,oder?", "Basic bWF1dmVTMzpibXgkMiswLTE9MSxvZGVyPw==")
+        checkAuthHeader("mauveS3", "mauveS3-test", "Basic bWF1dmVTMzptYXV2ZVMzLXRlc3Q=")
+    }
+
+    private fun checkAuthHeader(username: String, password: String, expHeaderValue: String) {
+        val authenticator = HTTPBasicAuthenticator(username, password)
 
         val headers = mock<MultivaluedMap<String, Any>>()
         val requestContext = mock<ClientRequestContext> {
-            on {this.headers} doReturn headers
+            on { this.headers } doReturn headers
         }
         authenticator.filter(requestContext)
-        
-        verify(headers).add("Authorization", "Basic cGV0ZXI6cGV0ZXJzUGFzc3dvcmQ=")
+
+        verify(headers).add("Authorization", expHeaderValue)
     }
+
 }
