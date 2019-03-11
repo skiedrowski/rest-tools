1091434f4e2d5fd7133503dd53105b17596c8698
sk <sk_dev@posteo.de>
2019-03-11 11:35:53 +0100

    migrate to Java EE 8 dependencies & gradle 5

--- versions.groovy
+++ versions.groovy
@@ -1,14 +1,14 @@
-cdi_api='1.1' //Java EE 7
-jaxrs_api='2.0' //Java EE 7
-javax_annotation = '1.2' //Java EE 7 
-javax_xml_bind = '2.2.12' //Java EE 7
+cdi_api='2.0' //Java EE 8
+jaxrs_api='2.1' //Java EE 8
+javax_annotation = '1.3' //Java EE 8 
+javax_xml_bind = '2.2.12' //Java EE 8
 
-kt_tools = '0.2'
-cdi_properties = '0.4'
+kt_tools = '0.5'
+cdi_properties = '0.5'
 
 //test
 junit = '4.+'
 hamkrest = '1.4.+'
 mockito = '2.9.+'
 mockito_kotlin = '1.5.+'
-payara='4.1.2.173.0.1'
+payara='5.191'
