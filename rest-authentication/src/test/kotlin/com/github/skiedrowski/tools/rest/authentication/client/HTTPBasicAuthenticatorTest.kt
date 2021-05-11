931bd6b94de9d95b10a65048cee6cd9f5eddf577
sk <sk_dev@posteo.de>
2021-05-11 20:34:38 +0200

    kotlin 1.5, release 20210511

--- rest-authentication/src/test/kotlin/com/github/skiedrowski/tools/rest/authentication/client/HTTPBasicAuthenticatorTest.kt
+++ rest-authentication/src/test/kotlin/com/github/skiedrowski/tools/rest/authentication/client/HTTPBasicAuthenticatorTest.kt
@@ -14,6 +14,11 @@ class HTTPBasicAuthenticatorTest {
     }
 
     @Test
+    fun `filter adds sensible Authorization header for Print`() {
+        checkAuthHeader("print", "meinKlartextPasswort", "Basic cHJpbnQ6bWVpbktsYXJ0ZXh0UGFzc3dvcnQ=")
+    }
+
+    @Test
     fun `filter adds sensible Authorization header 2`() {
         checkAuthHeader("S3", "hansS3-dummy", "Basic UzM6aGFuc1MzLWR1bW15")
         checkAuthHeader("S3", "hansS3-test", "Basic UzM6aGFuc1MzLXRlc3Q=")
